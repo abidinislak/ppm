e3172a3467977cf8316cd164d24892d90590cf2b
abidin <abidinislak@gmail.com>
2020-03-01 12:15:36 +0300

    first commit

---
+++ ppm/src/main/java/io/abidinppt/PersonelProjetManagementApplication.java
@@ -0,0 +1,13 @@
+package io.abidinppt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PersonelProjetManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PersonelProjetManagementApplication.class, args);
+	}
+
+}
---
+++ ppm/src/test/java/io/abidinppt/PersonelProjetManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package io.abidinppt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PersonelProjetManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
